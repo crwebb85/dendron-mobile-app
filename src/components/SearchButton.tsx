5bfb5862aaa3fccf77e62e3aa1b2fc0cd788dbfb
crwebb85 <crwebb85@gmail.com>
2022-01-27 20:25:03 -0500

    Added a search button.

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
+
 import { GestureResponderEvent } from 'react-native';
-import React from 'react';
+import { ToggleButton } from '@mui/material';
 import { VscSearch } from 'react-icons/vsc';
 
 export type ButtonProps = {
   onPress: ((event: GestureResponderEvent) => void) | undefined;
 };
 
-export const SearchButton = ({ onPress = undefined }: ButtonProps) => <VscSearch></VscSearch>;
+export const SearchButton = ({ onPress = undefined }: ButtonProps) => {
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <ToggleButton
+      value="check"
+      selected={selected}
+      onChange={() => {
+        setSelected(!selected);
+      }}
+    >
+      <VscSearch></VscSearch>
+    </ToggleButton>
+  );
+};
